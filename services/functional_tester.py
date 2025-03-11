45996e34f4ae884282c53e793411753bb3df996f
JANARTHANANPALANIVEL <janajana162004@gmail.com>
2025-03-12 00:44:05 +0530

    fixed

--- services/functional_tester.py
+++ services/functional_tester.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import os
+import datetime
+
 
 class FunctionalTester:
     def __init__(self):
@@ -12,7 +14,7 @@ class FunctionalTester:
         
     def setup_gemini(self):
         genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
-        self.model = genai.GenerativeModel('gemini-pro')
+        self.model = genai.GenerativeModel("gemini-1.5-pro-latest")  
         
     def setup_selenium(self):
         chrome_options = Options()
@@ -54,7 +56,7 @@ class FunctionalTester:
             results['passed'].append('Page Load Test')
             
             # Take screenshot
-            screenshot_path = f'static/screenshots/{datetime.now().strftime("%Y%m%d_%H%M%S")}.png'
+            screenshot_path = f'static/screenshots/{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}.png'
             self.driver.save_screenshot(screenshot_path)
             results['screenshots'].append(screenshot_path)
             
